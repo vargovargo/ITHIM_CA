b44c180b47e8c4bd3ec83f5185c8d0fb70157578
vargo <jvargo@gmail.com>
2017-10-23 08:52:22 -0700

    read xl

--- readCalibration.R
+++ readCalibration.R
@@ -3,40 +3,12 @@ library("devtools")
 install_github("ITHIM/ITHIM", ref="devel", force=TRUE)
 library("ITHIM")
 
-csvFileName <- "./ITHIM/CA_ITHIM_tests/SANDAG_ITHIM.csv"
 xlFileName <- "./ITHIM/fromNeil/ITHIM_California2016-12-12SANDAG_Trends.xlsx"
 
-SANDAG <- read.csv(csvFileName, header = T, stringsAsFactors = FALSE)
-SANDAG <- readxl::read_excel(xlFileName, sheet="Calibration Data") 
+ITHIMss <- readxl::read_excel(xlFileName, sheet="Calibration Data") 
 
-
-
-# colClasses = c("integer",
-#                "integer",
-#                "integer",
-#                "character",
-#                "character",
-#                "integer",
-#                "character",
-#                "character",
-#                "character",
-#                "integer",
-#                "factor",
-#                "factor",
-#                "character",
-#                "character",
-#                "factor",
-#                "character",
-#                "factor",
-#                "numeric",
-#                "numeric",
-#                "numeric",
-#                "numeric",
-#                "numeric",
-#                "date",
-#                "character",
-#                "character",
-#                "character")
 
 
 holder <- createITHIM()
+
+getParameterSet(holder)
